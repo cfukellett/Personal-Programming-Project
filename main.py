9fd0d054afad14f3eb5980b038d4d499c8212689
cfukellett <cfu@kellettschool.com>
2026-03-13 06:40:00 +0000

    Mar 13, 2026, 2:40 PM

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import time
 def intro():
     print("--Welcome--")
     time.sleep(1.2)
-    player_name = input("Please enter your name:\n")
-    print(f"Hello, {player_name}, welcome to the game.")
+    name = input("Please enter your name:\n")
+    print(f"Hello, {name}, welcome to the game.")
     time.sleep(1)
     read_rules = input("Would you like to read the rules? (y/n):\n").lower()
     while read_rules not in ['y', 'yes', 'n', 'no']:
@@ -14,28 +14,23 @@ def intro():
     if read_rules in ['y', 'yes']:
         print("rules here")
     print("The game will now begin.")
-    roles()
+    return name
 
 def roles():
-    comp1_role = ""
-    comp2_role = ""
-    comp3_role = ""
-    comp4_role = ""
-    comp5_role = ""
-    comp6_role = ""
-    comps = [comp1_role, comp2_role, comp3_role, comp4_role, comp5_role, comp6_role]
-    player_role = randint(0,1)
-    if player_role == 0:
-        player_role = "surv"
+    comp1 = ""
+    comp2 = ""
+    comp3 = ""
+    comp4 = ""
+    comp5 = ""
+    comp6 = ""
+    comps = [comp1, comp2, comp3, comp4, comp5, comp6]
+    player = randint(0,1)
+    if player == 0:
+        player = "surv"
     else:
-        player_role = "murd"
-    if player_role == "murd":
-        comp1_role = "surv"
-        comp2_role = "surv"
-        comp3_role = "surv"
-        comp4_role = "surv"
-        comp5_role = "surv"
-        comp6_role = "surv"
+        player = "murd"
+    if player == "murd":
+        comp1 = comp2 = comp3 = comp4 = comp5 = comp6 = "surv"
     else:
         random_murd = randint(1, 6)
         for i in range(len(comps)):
@@ -52,10 +47,12 @@ def roles():
         time.sleep(0.3)
     time.sleep(1)
     print(" " * len(select), end='\r')
-    if player_role == "murd":
+    if player == "murd":
         print("🔪 murderer.")
     else:
         print("😨survivor.")
+
+    return player, comp1, comp2, comp3, comp4, comp5, comp6
 
 def energy(pts):
     if pts >= 200:
