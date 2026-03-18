99872268199d81ca65692dc110a9f584cb96a37c
cfukellett <cfu@kellettschool.com>
2026-03-18 04:00:53 +0000

    Mar 18, 2026, 12:00 PM

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ def roles():
     else:
         print("😨survivor.\n")
     time.sleep(2)
+    #print(player, comp1, comp2, comp3, comp4, comp5, comp6)
     return player, comp1, comp2, comp3, comp4, comp5, comp6
 
 def energy(pts):
