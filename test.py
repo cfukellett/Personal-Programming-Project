bc1326b8e24c17d6d24d3bfe4268fb148761b486
cfukellett <cfu@kellettschool.com>
2026-04-10 10:17:22 +0000

    Apr 10, 2026, 6:17 PM

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+from random import randint
 peektext = [f"...and saw here walking on the streets with their hands in their pockets...", "and test"]
 peektext2 = ["...and saw here"]
 peektext3 = peektext
 peektext3 += peektext2
 print(peektext3)
 allpeektextindex = len(peektext3)-1
-print(f"{peektext3[allpeektextindex]}")
+test = randint(1, allpeektextindex)
+print(f"{peektext3[test]}")
