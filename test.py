7b12ce15ea2e7180d4b3ed901e8c7a79aa05d7c2
cfukellett <cfu@kellettschool.com>
2026-04-10 10:13:06 +0000

    Apr 10, 2026, 6:13 PM

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ peektext2 = ["...and saw here"]
 peektext3 = peektext
 peektext3 += peektext2
 print(peektext3)
-allpeektextindex = peektext3.count()
+allpeektextindex = len(peektext3)
 print(f"{peektext3[allpeektextindex]}")
