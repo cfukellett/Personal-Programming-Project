6f6bb85f23e28badf5c388bd6721c406a92220cc
cfukellett <cfu@kellettschool.com>
2026-04-10 09:04:08 +0000

    Apr 10, 2026, 5:04 PM

---
+++ test.py
@@ -0,0 +1,4 @@
+peektext = [f"...and saw here walking on the streets with their hands in their pockets...", "and test"]
+peektext2 = ["...and saw here"]
+peektext3 = peektext +peektext2
+print(peektext3)
