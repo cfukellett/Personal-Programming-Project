a5a74b397ec998c25e53ccdb56bc5fbd50b820a0
cfukellett <cfu@kellettschool.com>
2026-04-15 04:47:42 +0000

    Apr 15, 2026, 12:47 PM

--- test.py
+++ test.py
@@ -8,6 +8,12 @@
 
 
 from random import randint
+deadlist = {"was found with his eyes gouged out and his neck hanging on a branch… Terrifying! 😱", "had been impaled and had died from blood loss...", "was electrocuted, leaving their body completely unrecognisable...", "was having a midnight snack and suddenly suffered from a heart attack...Yes, this was caused by the murderer.", "somehow found an active volcano and jumped into it, burning themselves into ashes in the process.", "was found without skin in a toolshed."}
+rand_death = randint(1,len(deadlist))
+print(rand_death)
+
+
+
 peektext = [f"...and found", "...and test"]
 peektext2 = ["...and saw here"]
 peektext3 = peektext
