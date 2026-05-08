48e468e37e11da943dd1f50e16d11ab7e451f18f
cfukellett <cfu@kellettschool.com>
2026-05-08 07:01:50 +0000

    May 8, 2026, 3:01 PM

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ def energy(pts):
 def botenergylv(name_list, murd, day):
     print(name_list)
     if day == 1:
-        botenergylist = ["", ""]
+        botenergylist = [0, 0]
         rep = False
         if murd != 0:
             name_list.pop(murd)
