4ba0e0f687557e50b28ce8f34e5fff0aa843ec6a
cfukellett <cfu@kellettschool.com>
2026-05-08 06:47:00 +0000

    May 8, 2026, 2:47 PM

--- test copy.py
+++ test copy.py
@@ -11,23 +11,23 @@ from random import randint
 name_list = ["Tom", "James", "Bob"]
 murd = 0
 botenergylist = []
+print(name_list[0])
 if murd != 0:
     name_list = name_list.pop(murd)
     print("done")
 name_list.pop(0)
 print(name_list)
 for i in range (3):
-    person = 0
     energyboost = 0
+    person = 0
     for name in name_list:
-        person += 1
         energyboost = 0
         status = randint(1,1)
         if status == 1:
             energyboost += 5
-            print(botenergylist)
             if len(botenergylist) != 0:
-                botenergylist[person-1] += energyboost
+                botenergylist[0] += energyboost
             else:
                 botenergylist.append(energyboost)
+        person += 1
     print(botenergylist)
