dfd45894175af994dc507448b24e8eb30fa87a5f
cfukellett <cfu@kellettschool.com>
2026-05-13 04:42:53 +0000

    May 13, 2026, 12:42 PM

--- main.py
+++ main.py
@@ -303,6 +303,7 @@ print(player_role)
 role_list = [player_role, comp1_role, comp2_role, comp3_role, comp4_role, comp5_role, comp6_role]
 murd, chosen = murdthing(player_role, role_list)
 dead = False
+print(name_list)
 while dead == False:
     day_num += 1
     energy_lv = energy(energy_points)
