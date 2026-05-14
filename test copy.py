19185ae3cac44d9b14dcbf9d9fe827fb97a18197
cfukellett <cfu@kellettschool.com>
2026-05-14 03:04:58 +0000

    May 14, 2026, 11:04 AM

--- test copy.py
+++ test copy.py
@@ -10,15 +10,35 @@
 from random import randint
 import random
 import time
+from colorist import ColorRGB, BgColorRGB, rgb, bg_rgb
 
-def dot_spam(text, remove):
+#ze colours
+def red(string):
+    rgb(string, 255,0,0)
+
+def blue(string):
+    rgb(string, 0,0,255)
+
+def green(string):
+    rgb(string, 0,255,0)
+
+def yellow(string):
+    rgb(string, 255,255,0)
+
+def purple(string):
+    rgb(string, 255,0,255)
+
+def dot_spam(text, remove, colour):
+    colours = {
+        "red" : red
+    }
     for i in range(5):
         text += "."
-        print(text, end='\r')
+        colour(text, end='\r')
         time.sleep(0.3)
     time.sleep(1)
     print(" " * len(text), end='\r')
     if not remove:
         print(text)
 
-dot_spam("The murderer has killed everyone except you.", False)
+dot_spam("The murderer has killed everyone except you.", False, "red")
