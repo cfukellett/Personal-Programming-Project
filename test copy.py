71adad051228f7f3fd30f513572460501b4d5d92
cfukellett <cfu@kellettschool.com>
2026-05-14 02:07:53 +0000

    May 14, 2026, 10:07 AM

--- test copy.py
+++ test copy.py
@@ -6,30 +6,17 @@
 
 
 
-from random import randint
 
-name_list = ["Tom", "James", "Bob"]
-murd = 0
-botenergylist = []
-print(name_list[0])
-if murd != 0:
-    name_list = name_list.pop(murd)
-    print("done")
-name_list.pop(0)
-print(name_list)
-rep = 0
-for i in range (3):
-    energyboost = 0
-    person = 0
-    for name in name_list:
-        energyboost = 0
-        status = randint(1,1)
-        if status == 1:
-            energyboost += 5
-            if rep != 0:
-                botenergylist[person] += energyboost
-            else:
-                botenergylist.append(energyboost)
-        person += 1
-    rep += 1
-    print(botenergylist)
+from random import randint
+import random
+import time
+
+def dot_spam(text):
+    for i in range(5):
+        text += "."
+        print(text, end='\r')
+        time.sleep(0.3)
+    time.sleep(1)
+    print(" " * len(text), end='\r')
+
+dot_spam("The murderer has killed everyone except you.")
