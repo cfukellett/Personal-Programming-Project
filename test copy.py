49e2f1af1156cdde900d01c74daeb94de2f64376
cfukellett <cfu@kellettschool.com>
2026-05-14 02:09:03 +0000

    May 14, 2026, 10:09 AM

--- test copy.py
+++ test copy.py
@@ -11,12 +11,13 @@ from random import randint
 import random
 import time
 
-def dot_spam(text):
+def dot_spam(text, remove):
     for i in range(5):
         text += "."
         print(text, end='\r')
         time.sleep(0.3)
     time.sleep(1)
-    print(" " * len(text), end='\r')
+    if remove == True:
+        print(" " * len(text), end='\r')
 
-dot_spam("The murderer has killed everyone except you.")
+dot_spam("The murderer has killed everyone except you.", False)
