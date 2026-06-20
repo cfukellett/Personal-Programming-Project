bf3276a7548030d11fef6b7ee51a1524550af933
cfukellett <cfu@kellettschool.com>
2026-06-20 14:25:35 +0000

    Jun 20, 2026, 10:25 PM

---
+++ test copy 2.py
@@ -0,0 +1,15 @@
+#hellohellohellohellohelloHiHiHihelloHi
+
+
+
+
+
+
+
+from random import randint
+
+namelist = ['Person 1', 'Person 2', 'Person 3']
+test = input()
+test = test.lower()
+if test in namelist.lower():
+    print("ok")
