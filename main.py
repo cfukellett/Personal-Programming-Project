ef4c66c2e5788cdb3ed4713f0a9d0af01f6af181
cfukellett <cfu@kellettschool.com>
2026-06-21 13:05:47 +0000

    Jun 21, 2026, 9:05 PM

--- main.py
+++ main.py
@@ -125,13 +125,15 @@ def vote(namelist, suspts, energylv, murd, playername, day, dayrandomvar):
     random.shuffle(defoptions)
     purple(f"You must interrogate every person.\nYou will be given three options.\nChoose the correct option for better odds of deducing who the murderer is.")
     if day <= 2:
+        green("Input anything to begin the interrogation!\n")
         ReallyRandomInputThatWillNeverBeUsed = input()
     else:
-        ReallyRandomInputThatWillNeverBeUsed = input("1. Start Interrogation\n2. I'm getting bored of this interrogation slop. SKIP!!!\n")
+        green("1. Start Interrogation\n2. I'm getting bored of this interrogation slop. SKIP!!!\n")
+        ReallyRandomInputThatWillNeverBeUsed = input()
         while ReallyRandomInputThatWillNeverBeUsed not in ['1', '2']:
             wronginsert()
-            ReallyRandomInputThatWillNeverBeUsed = input("1. Start Interrogation\n2. I'm getting bored of this interrogation slop. SKIP!!!\n")
-    
+            green("1. Start Interrogation\n2. I'm getting bored of this interrogation slop. SKIP!!!\n")
+            ReallyRandomInputThatWillNeverBeUsed = input()
     if ReallyRandomInputThatWillNeverBeUsed == '1':
         for person in intenamelist:
             blue(f"{person} is being interrogated.")
