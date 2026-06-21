3c259b01870730a17ff88ebd53050fccf4339eb9
cfukellett <cfu@kellettschool.com>
2026-06-21 13:40:31 +0000

    Jun 21, 2026, 9:40 PM

--- main.py
+++ main.py
@@ -459,7 +459,10 @@ def day(day, suspts, energylv, name_list, murd, player_role, chosen, player_name
     yellow(f"☀️--Day {day}--☀️")
     purple("It's the start of a brand new day.")
     purple(f"Here are your day {day} stats:")
-    green(f"Your role: {player_role}")
+    if player_role == 'surv':
+        blue(f"Your role: 😨survivor")
+    else:
+        red(f"Your role: 🔪 murderer")
     blue(f"🤔 Suspicion Points: {suspts}")
     yellow(f"⚡Energy Level: {energylv}")
     deadlist = ["was found with his eyes gouged out and his neck hanging on a branch… Terrifying! 😱", "had been impaled and had died from blood loss...", "was electrocuted, leaving their body completely unrecognisable...", "was having a midnight snack and suddenly suffered from a heart attack...Yes, this was caused by the murderer.", "somehow found an active volcano and jumped into it, burning themselves into ashes in the process.", "was found without skin in a toolshed."]
