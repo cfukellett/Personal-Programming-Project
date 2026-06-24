3b348b115e1189ce50eab9a82ba3ab20460694af
cfukellett <cfu@kellettschool.com>
2026-06-24 02:45:25 +0000

    Jun 24, 2026, 10:45 AM

--- jjk.py
+++ jjk.py
@@ -8,8 +8,8 @@ import sys
 import os
 import subprocess
 pygame.mixer.init()
-#pygame.mixer.music.load("bgm.mp3")
-#pygame.mixer.music.play(-1)
+pygame.mixer.music.load("bgmjjk.mp3")
+pygame.mixer.music.play(-1)
 
 #ze colours
 def red(string):
